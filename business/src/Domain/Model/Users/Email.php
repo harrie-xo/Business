1f15c893a2ce93e333d76366f6de1a701cd399ae
harrieanto <harrie_xo@yahoo.co.id>
2018-01-06 20:31:20 +0700

    Add files via upload

---
+++ business/src/Domain/Model/Users/Email.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Component\Packages\Business\Domain\Model\Users;
+ 
+use Assert\Assertion;
+ 
+class Email
+{
+    /**
+     *
+     * @var string
+     *
+     */
+    private $value;
+
+    /**
+     *
+     * Create a new Email
+     *
+     * @param string $value
+     * @return void
+     *
+     */
+    public function __construct($value)
+    {
+        Assertion::email($value);
+        $this->value = $value;
+    }
+
+    /**
+     * Return the object as a string
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->value;
+    }
+}
